2fc266f7c92fc5543b653134647ec6caa1fa530f
Naavid Morshed <naavid.m@gmail.com>
2023-12-04 18:48:18 +0600

    fix sb transient error

--- shop/src/main/java/com/example/shop/ShopApplication.java
+++ shop/src/main/java/com/example/shop/ShopApplication.java
@@ -2,6 +2,9 @@ package com.example.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShopApplication {
@@ -10,4 +13,13 @@ public class ShopApplication {
         SpringApplication.run(ShopApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+            }
+        };
+    }
 }
